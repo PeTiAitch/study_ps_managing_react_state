8d68237093673bccf29de5bb482b12e8312b5784
plam <vvillbes@gmail.com>
2021-05-18 12:04:17 +0300

    03_07 Implementing Derived State and Exploring When React Renders

--- src/App.js
+++ src/App.js
@@ -57,6 +57,10 @@ export default function App() {
     );
   }
 
+  const filteredProducts = size
+    ? products.filter((p) => p.skus.find((s) => s.size === parseInt(size)))
+    : products;
+
   return (
     <>
       <div className="content">
@@ -74,8 +78,9 @@ export default function App() {
               <option value="8">8</option>
               <option value="9">9</option>
             </select>
+            {size && <h2>Found {filteredProducts.length} items</h2>}
           </section>
-          <section id="products">{products.map(renderProduct)}</section>
+          <section id="products">{filteredProducts.map(renderProduct)}</section>
         </main>
       </div>
       <Footer />
