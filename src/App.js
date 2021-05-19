e9a7b569695f3446394ec38a18f52f22177bd03e
plam <vvillbes@gmail.com>
2021-05-19 13:18:19 +0300

    04_04 Declaring Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
 import Products from "./Products";
+import Detail from "./Detail";
+import Cart from "./Cart";
 
 export default function App() {
   return (
@@ -11,7 +14,11 @@ export default function App() {
       <div className="content">
         <Header />
         <main>
-          <Products />
+          <Routes>
+            <Route path="/" element={<Products />} />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
         </main>
       </div>
       <Footer />
