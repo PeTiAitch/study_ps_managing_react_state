487b33b46bb3db478487d5dd246c67c2d3d45333
plam <vvillbes@gmail.com>
2021-05-21 13:44:51 +0300

    05_03 Implementing Derived State

--- src/Detail.js
+++ src/Detail.js
@@ -30,7 +30,11 @@ export default function Detail() {
       </select>
 
       <p>
-        <button className="btn btn-primary" onClick={() => navigate("/cart")}>
+        <button
+          className="btn btn-primary"
+          onClick={() => navigate("/cart")}
+          disabled={!sku}
+        >
           Add to cart
         </button>
       </p>
