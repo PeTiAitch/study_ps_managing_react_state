63d79292b248d205e6b9f32b8b11d287f2545094
plam <vvillbes@gmail.com>
2021-05-25 14:08:53 +0300

    07_05 Avoiding Setting State on Unmounted Components

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,28 +1,38 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 const useFetch = (url) => {
+  const isMountedRef = useRef(false);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    isMountedRef.current = true;
     const init = async () => {
       try {
         const response = await fetch(`${baseUrl}${url}`);
         if (response.ok) {
           const json = await response.json();
-          setData(json);
+          if (isMountedRef.current) {
+            setData(json);
+          }
         } else {
           throw response;
         }
       } catch (e) {
-        setError(e);
+        if (isMountedRef.current) {
+          setError(e);
+        }
       } finally {
-        setLoading(false);
+        if (isMountedRef.current) {
+          setLoading(false);
+        }
       }
     };
     init();
+
+    return () => (isMountedRef.current = false);
   }, [url]);
 
   return { data, error, loading };
