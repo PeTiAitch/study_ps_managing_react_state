c526327a7a4a22565b5ffdcd5511dea35609ba64
plam <vvillbes@gmail.com>
2021-05-26 14:45:47 +0300

    08_06 Handling Updates In Reducers

--- src/cartReducer.js
+++ src/cartReducer.js
@@ -16,6 +16,12 @@ export default function cartReducer(cart, action) {
         return [...cart, { id, sku, quantity: 1 }];
       }
     }
+    case "updateQuantity": {
+      const { sku, quantity } = action;
+      return quantity === 0
+        ? cart.filter((i) => i.sku !== sku)
+        : cart.map((i) => (i.sku === sku ? { ...i, quantity } : i));
+    }
     default:
       throw new Error("Unhandled action " + action.type);
   }
